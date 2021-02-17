3f04ccd1091a95207d665f1873d90c9c708f488d
ovi <ovidiu.opreah@yahoo.com>
2021-02-17 23:37:25 +0000

    two complex

--- hello_git.py
+++ hello_git.py
@@ -3,3 +3,9 @@ print("Hello Git world!")
 list = ['a', 'b', 'c']
 for n in list:
     print(f"This is letter {n}. ")
+
+list2 = ['x', 'y', 'z']
+
+for m in list2:
+    for n in list:
+        print(f"{m} and {n};")
