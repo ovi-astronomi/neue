69f8f30928c884677ce9120d54d61c1aed6fe667
ovi-astronomi <72999738+ovi-astronomi@users.noreply.github.com>
2021-02-19 19:01:22 +0000

    frequency of rolls

--- instance.py
+++ instance.py
@@ -5,8 +5,14 @@ die = Die(16)
 tally = []
 
 # roll the dice 10 times:
-for roll_count in range(10):
+for roll_count in range(1000):
     x = die.roll()
     tally.append(x)
 
 print(tally)
+
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = tally.count(value)
+    frequencies.append(frequency)
+print(frequencies)
